02085ffa8476424c62235570d2c98c49a5dd0763
Lloyd <lloyd.williams1062@gmail.com>
2025-06-23 02:34:04 +1000

    🙃

--- flake.nix
+++ flake.nix
@@ -1,46 +1,85 @@
 {
-  description = "OxCaml dev shell";
+  description = "Flake to build OxCaml from source, with a dev shell including Dune 3";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    oxcaml = {
-      url = "github:oxcaml/oxcaml";
-      flake = false;
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # Optionally pin a specific revision to ensure dune_3 is available
+    # nixpkgs.url = "github:NixOS/nixpkgs/your-commit-hash";
   };
 
-  outputs = { self, nixpkgs, oxcaml }: 
+  outputs = { self, nixpkgs, ... }:
     let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          (final: prev: {
-            oxcaml = prev.stdenv.mkDerivation {
-              pname = "oxcaml";
-              src = oxcaml;
-              inherit (prev.ocaml) ocaml;  # Use OCaml compiler
-              buildInputs = [ prev.ocaml ]; 
-              installPhase = ''
-                mkdir -p $out/bin
-                cp _build/default/bin/ocamlc.opt $out/bin/ocamlc
-                cp _build/default/bin/ocamlopt.opt $out/bin/ocamlopt
-              '';
-            };
-          })
-        ];
-      };
+      system = "x86_64-linux";  # or "aarch64-linux"
+      pkgs = nixpkgs.legacyPackages.${system};
+      lib = pkgs.lib;
+      ocamlPkgs = pkgs.ocamlPackages;
     in {
-      devShells.${builtins.currentSystem} = pkgs.mkShell {
-        buildInputs = [
-          pkgs.oxcaml
-          pkgs.ocamlPackages.dune
-          pkgs.ocamlPackages.merlin
-          pkgs.ocamlPackages.ocaml-lsp-server
+      packages.${system}.oxcaml = pkgs.stdenv.mkDerivation {
+        pname = "oxcaml";
+        version = "main";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "oxcaml";
+          repo = "oxcaml";
+          rev = "main";
+          sha256 = lib.fakeSha256;
+        };
+
+        nativeBuildInputs = [
+          pkgs.autoconf
+          pkgs.automake
+          pkgs.libtool
+          pkgs.m4
+          pkgs.pkg-config
         ];
-        shellHook = ''
-          echo "Use ocamlc/ocamlopt from OxCaml"
+
+        buildInputs = [
+          ocamlPkgs.ocaml
+          # Use Dune 3 for any parts that invoke dune
+          (builtins.tryEval ''${toString ocamlPkgs.dune_3}'' // { success = true; value = ocamlPkgs.dune; })
+          # If dune_3 attribute exists, prefer that; else fallback to ocamlPkgs.dune
+          ocamlPkgs.menhir
+        ];
+
+        configurePhase = ''
+          autoreconf -ivf
+          ./configure \
+            --prefix=$out \
+            --enable-runtime5
+        '';
+
+        buildPhase = ''
+          make world.opt
+        '';
+        installPhase = ''
+          make install
         '';
       };
+
+      devShells.${system}.default = pkgs.mkShell {
+        # Development dependencies: OCaml compiler, Dune 3, and build tools
+        buildInputs = [
+          ocamlPkgs.ocaml
+          # Prefer dune_3 if available; otherwise use dune
+          (if builtins.hasAttr "dune_3" ocamlPkgs then ocamlPkgs.dune_3 else ocamlPkgs.dune)
+          ocamlPkgs.findlib
+          ocamlPkgs.merlin        # for editor integration, if desired
+          pkgs.pkg-config
+          pkgs.autoconf
+          pkgs.automake
+          pkgs.libtool
+          pkgs.m4
+        ];
+
+        # Optional: set environment variables or hints
+        shellHook = ''
+          echo "OCaml $(ocamlc -version) available, Dune version: $(dune --version)"
+          echo "To build OxCaml: nix build .#oxcaml"
+        '';
+      };
+
+      # Expose default package
+      defaultPackage = self.packages.${system}.oxcaml;
+      defaultDevShell = self.devShells.${system}.default;
     };
 }
-
